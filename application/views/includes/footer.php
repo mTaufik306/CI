7dc197fe3e7d32cd51f6ceeed7d5ef96582dcc24
taufik <taufik@varnion.com>
2013-11-10 10:54:46 +0700

    add some adjustment for day 6: - delete attachment directory - delete css directory - add the controller, view, and template
    Conflicts:
    	css/style.css

---
+++ application/views/includes/footer.php
@@ -0,0 +1,12 @@
+<!--notice that no opening tag of body because this is footer page-->
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>	
+
+	<script type="text/javascript" charset="utf-8">
+		$('input').click(function(){
+			$(this).select();	
+		});
+	</script>
+
+</body>
+</html>
---
+++ application/views/includes/template.php
@@ -0,0 +1,10 @@
+<?php $this->load->view('includes/header'); ?>
+
+<!--rather than referencing an absolute path, it should go with a variable
+	because the content of a page should be relative
+	this variable will be passed by controller who load the page
+	and should be passed as '$data[main_content]'
+-->
+<?php $this->load->view($main_content); ?>
+
+<?php $this->load->view('includes/footer'); ?>
