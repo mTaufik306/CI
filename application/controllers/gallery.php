e60dde16b69f821ac884ac92ca618425fef03052
taufik <taufik@varnion.com>
2013-11-11 16:58:57 +0700

    completing the controller and its corresponding view //controller load model and leave the rest to the model //controller simply do upload right after the client click the submit //controller also retrieve existing image(s) and pass it to its corresponding view //the view simply have 2 div: //1st div: image gallery, at this part the view will load the image (if exist) //which is passed by the controller //if there hasn't been any image,the view just simply say "please upload an image //2nd div: image upload section just like the previous commit

--- application/controllers/gallery.php
+++ application/controllers/gallery.php
@@ -3,9 +3,15 @@ class Gallery extends CI_Controller {
 	
 	function index() {
 		
+		$this->load->model('Gallery_model');
 		
+		if ($this->input->post('upload')) {
+			$this->Gallery_model->do_upload();
+		}
 		
-		$this->load->view('gallery');
+		$data['images'] = $this->Gallery_model->get_images();
+		
+		$this->load->view('gallery', $data);
 		
 	}
 	
