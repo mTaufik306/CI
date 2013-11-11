6b15ae2b56ca3bb6841f5ddde316a28b1042ccbd
taufik <taufik@varnion.com>
2013-11-11 12:56:00 +0700

    remove ajax(javascript) load script from footer and place it into header //because the script will be used by main content which is below header //and above footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,9 +1,11 @@
 <!--notice that no opening tag of body because this is footer page-->
 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>	
-
 	<script type="text/javascript" charset="utf-8">
 		$('input').click(function(){
+			$(this).select();	
+		});
+
+		$('textarea').click(function(){
 			$(this).select();	
 		});
 	</script>
