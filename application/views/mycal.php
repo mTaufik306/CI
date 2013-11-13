0c57d56ff0eef02af284e5837559ebc4cffb79df
taufik <taufik@varnion.com>
2013-11-13 15:43:27 +0700

    modify mycal_model: -add global var conf //for the cal. config. -create model constructor -function generate modified to generate the template for the calendar //add calendar class to the table open tag modify mycal view: -add style to have the calendar on the middle of the screen //with arial font style

--- application/views/mycal.php
+++ application/views/mycal.php
@@ -2,6 +2,14 @@
 <html lang="en">
 <head>
 	<title>Day 10 (Calendar)</title>
+	<style type="text/css" />
+		.calendar {
+			font-family: Arial; font-size: 12px;
+		}
+		table.calendar{
+			margin: auto; border-collapse: collapse;
+		}
+	</style>
 </head>
 <body>
 	<?php echo $calendar?>
