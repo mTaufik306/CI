657cec8d5c3cd5bbd8ad582c327d6925dc638be0
taufik <taufik@varnion.com>
2013-11-13 14:39:04 +0700

    reorganize the calendar into mvc model: -creating model (mycal_model) along with its function named 'generate' //this function takes 2 param:  year<yyyy>, month<mm> //this function calls calendar library and generate it //this function return the result of calendar->generate() -creating view to present the calendar //the controller passes data with array 'calendar' //the view just echoing 'calendar' nothing's changed since the last commit except for the file organization

--- application/controllers/mycal.php
+++ application/controllers/mycal.php
@@ -2,17 +2,15 @@
 class Mycal extends CI_Controller {
 	
 	function display() {
+
 		$year = $this->uri->segment(3);
 		$month = $this->uri->segment(4);
-		$config = array(
-			'start_day' => 'monday',
-			'show_next_prev' =>true,
-			'next_prev_url' => base_url().'mycal/display'
-		);
 
+		$this->load->model('mycal_model');
+		$data['calendar']=$this->mycal_model->generate($year,$month);
+
+		$this->load->view('mycal',$data);
 		
-		$this->load->library('calendar',$config);
-		echo $this->calendar->generate($year,$month);
 	}
 	
 }
---
+++ application/views/mycal.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Day 10 (Calendar)</title>
+</head>
+<body>
+	<?php echo $calendar?>
+</body>
+</html>
