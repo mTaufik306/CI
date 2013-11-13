92c6926dd013f0e0780d34a59b0bae917228ef8e
taufik <taufik@varnion.com>
2013-11-13 14:17:22 +0700

    create config param for the calendar within controller mycal:: -changing the starting day of the month //to monday -add link to prev,next month -add params year and month to be included with calendar->generate(year,month)

--- application/controllers/mycal.php
+++ application/controllers/mycal.php
@@ -2,10 +2,17 @@
 class Mycal extends CI_Controller {
 	
 	function display() {
+		$year = $this->uri->segment(3);
+		$month = $this->uri->segment(4);
+		$config = array(
+			'start_day' => 'monday',
+			'show_next_prev' =>true,
+			'next_prev_url' => base_url().'mycal/display'
+		);
+
 		
-		$this->load->library('calendar');
-		echo $this->calendar->generate();
-		
+		$this->load->library('calendar',$config);
+		echo $this->calendar->generate($year,$month);
 	}
 	
 }
