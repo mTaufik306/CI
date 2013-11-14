ae48a3d435d4fbe68fb5cd003514a173bb1bb3ec
taufik <taufik@varnion.com>
2013-11-14 13:00:48 +0700

    add new function into controller cart_test: -show  .show existing item(s) in cart -add2  .add 1 item (hard-coded) -update  .update 1 item in cart using rowid -total  .get total cost/price of existing item -remove  .remove specific item in cart -destroy  .remove all items in cart

--- application/controllers/cart_test.php
+++ application/controllers/cart_test.php
@@ -17,4 +17,51 @@ class Cart_test extends CI_Controller{
 		echo "add() called";
 	}
 
+	function show() {
+		$cart = $this->cart->contents();
+		echo "<pre>";
+			print_r($cart);
+		
+	}
+
+	function add2() {
+		$data = array(
+			'id' => '32',
+			'name' => 'Trouser',
+			'qty' => 10,
+			'price' => 150.00,
+			'options' => array('Size' => 'medium') 
+		);
+		$this->cart->insert($data);
+		echo "add2() called";		
+	}
+
+	function update() {
+
+		$data = array(
+			'rowid' => '6340d5159197cc92881e9570b81540fd',
+			'qty' => 12
+		);
+		$this->cart->update($data);
+	}
+
+	function total() {
+		echo $this->cart->total();
+	}
+
+	function remove() {
+		$data = array(
+			'rowid' => '6340d5159197cc92881e9570b81540fd',
+			'qty' => 0
+		);
+		$this->cart->update($data);
+		echo "deleted";
+
+	}
+
+	function destroy() {
+		$this->cart->destroy();
+		echo "destroyed";
+	}
+
 }
