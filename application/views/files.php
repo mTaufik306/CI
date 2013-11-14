8c6b768fb3b608e9925e2aa8e3b02c7ab5328afd
taufik <taufik@varnion.com>
2013-11-14 11:22:35 +0700

    .create view untuk menyajikan data directory menggunakan ajax .load jquery 1.4.2/jquery.min.js .dibuatnya fungsi jquery untuk mengexpand directory apabila diklik

---
+++ application/views/files.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Files</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head>
+<body>
+	<?php //print_r($files); ?>
+	<div id="files">
+		
+	</div>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			var files = <?php echo json_encode($files); ?>;
+			var file_tree = build_file_tree(files);
+			file_tree.appendTo('#files');
+			
+			function build_file_tree(files) {
+				
+				var tree = $('<ul>');
+				
+				for (x in files) {
+					
+					if (typeof files[x] == "object") {
+						var span = $('<span>').html(x).appendTo(
+							$('<li>').appendTo(tree).addClass('folder')
+						);
+						var subtree = build_file_tree(files[x]).hide();
+						span.after(subtree);
+						span.click(function() {
+							$(this).parent().find('ul:first').toggle();
+						});
+						
+					} else {
+						$('<li>').html(files[x]).appendTo(tree).addClass('file');
+					}
+					
+				}
+				
+				return tree;
+				
+			}
+		});
+	
+	</script>
+</body>
+</html>
